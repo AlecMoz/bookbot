3042c8de59c0f63e29ba2be5079889566ca5371f
AlecMoz <alexandermozingo@gmail.com>
2025-08-18 14:46:58 -0500

    added ordered count

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from stats import word_count
 
 from stats import character_count
 
+from stats import sort_character
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -13,8 +15,17 @@ def main():
     book_text = get_book_text(filepath)
     num_words = word_count(book_text)
     count = character_count(book_text)
-    print(f"{num_words} words found in the document")
-    print(count)
+    ordered_count = sort_character(count)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("------- Character Count ---------")
+    for item in ordered_count:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
+    
 
 
 
--- stats.py
+++ stats.py
@@ -11,3 +11,17 @@ def character_count(book_text):
             count[letter] = 0
         count[letter] += 1
     return count
+
+
+def get_num(dict):
+    return dict["num"]
+
+
+def sort_character(count):
+    ordered_count = []
+    for char, num in count.items():
+        new_dict = {"char": char, "num": num}
+        if new_dict["char"].isalpha():
+            ordered_count.append(new_dict)
+    ordered_count.sort(reverse=True, key=get_num)
+    return ordered_count
